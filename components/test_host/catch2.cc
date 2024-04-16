7119ebf084d09efe56f7d4416fd7ef24223f8a2e
zwiebert <bertw@online.de>
2024-04-16 22:32:20 +0200

    Move components out. Fix tests

--- components/test_host/catch2.cc
+++
@@ -1,5 +0,0 @@
-//#include <catch2/internal/catch_default_main.hpp>
-
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
--- components/test_host/test_runner/mock.cc
+++
@@ -1,7 +0,0 @@
-#include <utils_misc/int_types.h>
-
-void  mcu_restart(void) {}
-bool mcu_get_buttonPin(void) { return false; }
-volatile uint32_t run_time_s_, run_time_ts_;
-void setup_pin(const struct cfg_gpio *c) {}
-void mcu_put_txPin(unsigned char) {}
