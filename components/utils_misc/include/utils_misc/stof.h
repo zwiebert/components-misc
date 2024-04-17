66f5693446e59fb8f61b811f9109b46119df1608
zwiebert <bertw@online.de>
2024-04-17 22:22:51 +0200

    Move out util-components

--- components/utils_misc/include/utils_misc/stof.h
+++
@@ -1,7 +0,0 @@
-#ifdef __cplusplus
-  extern "C" {
-#endif
-float stof(const char* s);
-#ifdef __cplusplus
-  }
-#endif
--- components/utils_misc/include/utils_misc/string_malloc.hh
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "allocator_malloc.hh"
-#include <string>
-
-
-using mstring = std::basic_string<char, std::char_traits<char>, AllocatorMalloc<char> >;
