978190e0faddb65cae13f79a9cb569c5d3c69c0d
zwiebert <bertw@online.de>
2024-04-23 21:14:45 +0200

    Move out component

--- components/stm32/host/stm32_impl.cc
+++
@@ -1,5 +0,0 @@
-#include <stm32/stm32_bootloader.hh>
-
-
-
-Stm32_Uart_if *stm32_uart;
--- components/stm32/include/stm32/stm32_flash.hh
+++
@@ -1,8 +0,0 @@
-/**
- * \file       stm32_flash.hh
- * \brief      download and update firmware of the connected STM32
- * \author     bertw
- */
-
-#pragma once
-#include <stm32/stm32_ota.h>
