636f89e1be04ca50ec8b7c2a6863ab8797465e2e
zwiebert <bertw@online.de>
2024-05-03 13:16:26 +0200

    Fix eof problem  in stdin

--- components/cli/cmdline_set.cc
+++ components/cli/cmdline_set.cc
@@ -9,6 +9,17 @@
 #include "cli_private.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#ifndef USE_STDIO_GETCHAR
+static int get_char() {
+  unsigned char c;
+  int n = read(0, &c, 1);
+  if (n != 1)
+    return -1;
+  return c;
+}
+#endif
 
 bool cliBuf_enlarge(struct cli_buf *buf) {
   unsigned new_size = buf->buf_size ? buf->buf_size + 32 : 64;
@@ -21,7 +32,6 @@ bool cliBuf_enlarge(struct cli_buf *buf) {
   return true;
 }
 
-
 char* get_commandline() {
   static struct cli_buf buf;
 
@@ -29,8 +39,13 @@ char* get_commandline() {
     if (!cliBuf_enlarge(&buf))
       return 0;
   for (;;) {
-    switch (cli_get_commandline(&buf, getchar)) {
-
+#ifdef USE_STDIO_GETCHAR
+  clearerr(stdin);
+  switch (cli_get_commandline(&buf, getchar))
+#else
+  switch (cli_get_commandline(&buf, get_char))
+#endif
+    {
     case CMDL_DONE:
       if (buf.buf[0] == '\0' || buf.buf[0] == '\r' || buf.buf[0] == '\n')
         return nullptr;
